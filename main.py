f931508c3b37643cc6e5cd53006791ea4575ab1d
Kill3rZack <zackthekill3r@outlook.com>
2022-07-03 22:29:15 -0300

    Core 0.6.0 | The Translations Update |
    Now supporting The Translations Update, and also with a bug fix of a crash that would happen whenever trying to download an unavailable resolution, even after the previous fix.

--- main.py
+++ main.py
@@ -5,13 +5,20 @@ import os
 import youtube_dl
 import time
 import re
+from settings import language
+
+if language == "English":
+    from Languages.english import *
+elif language == "Portuguese":
+    from Languages.portuguese import *
+elif language == "Japanese":
+    from Languages.japanese import *
 
 def clearconsole():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-
 root = os.path.dirname(__file__) 
-core_version="Alpha 0.5.1"
+core_version="Alpha 0.6.0"
 
 
 def yt_title_converter(title): # This makes sure the filename doesn't piss the OS and also Python off
@@ -38,61 +45,67 @@ def AutoDownload(url,AudioOnly=type(None),UseCaptions=type(None),CaptionsLanguag
     yt = YouTube(url)
     if AudioOnly == True:
         stream = yt.streams.get_by_itag(140)
-        print("Approximate size:")
-        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx)} | Will now be downloaded!")
+        print(Main[0])
+        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx)} | {Main[1]}")
         ts = time.perf_counter()
         if Filename == type(None):
             Filename=f"{yt.title}"
             Filename=f"{yt_title_converter(title=Filename)}.mp3"
         stream.download(filename=Filename,output_path=f"{root}/Downloads")
         tf = time.perf_counter()
-        print(f"Done in {tf - ts:0.4f} seconds!")
+        timer_result = Main[2].replace('/s', f"{tf - ts:0.4f}")
+        print(timer_result)
     else:
         stream = yt.streams.get_highest_resolution()
         ts = time.perf_counter()
         if Filename == type(None):
             Filename=f"{yt.title}"
             Filename=f"{yt_title_converter(Filename)}.mp4"
-        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx)} | Will now be downloaded!")
+        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx)} | {Main[1]} ")
         stream.download(filename=Filename,output_path=f"{root}/Downloads")
         tf = time.perf_counter()
-        print(f"Done in {tf - ts :0.4f} seconds!")
+        timer_result = Main[2].replace('/s', f"{tf - ts:0.4f}")
+        print(timer_result)
     return Filename
 
 def Download(url,AudioOnly=type(None),UseCaptions=type(None),CaptionsLanguage="en",Filename=type(None)): #Regular Download (Asks for quality)
     yt = YouTube(url)
     if AudioOnly == True:
         stream = yt.streams.get_by_itag(140)
-        print("Approximate size:")
-        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx)} | Will now be downloaded!")
+        print(Main[0])
+        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx)} | {Main[1]}")
         ts = time.perf_counter()
         if Filename == type(None):
             Filename=f"{yt.title}"
             Filename=f"{yt_title_converter(title=Filename)}.mp3"
         stream.download(filename=Filename,output_path=f"{root}/Downloads")
         tf = time.perf_counter()
-        print(f"Done in {tf - ts:0.4f} seconds!")
+        timer_result = Main[2].replace('/s', f"{tf - ts:0.4f}")
+        print(timer_result)
     else:
         print("| 144p |")
         print("| 360p |")
         print("| 480p |")
         print("| 720p |")
         print("| 1080p |")
-        ic = input("Select your desired quality ")
+        ic = input(Main[3])
         if "144" in ic:
             Uitag=17
+            split = False
         elif "360" in ic:
             Uitag=22
+            split = False
         elif "480" in ic:
             Uitag=135
             split = True
         elif "720" in ic:
             Uitag=22
+            split = False
         elif "1080" in ic:
             Uitag=248
             split = True
         else:
-            print("Not avaiable | Error Code: Err1nQ |")
+            print(Main[4])
             return "Err1NQ"
         stream = yt.streams.get_by_itag(Uitag)
         audio = yt.streams.get_by_itag(140)
@@ -102,7 +115,11 @@ def Download(url,AudioOnly=type(None),UseCaptions=type(None),CaptionsLanguage="e
             Raw_Filename=f"{yt_title_converter(Filename)} raw.mp4"
             Filename=f"{yt_title_converter(Filename)}.mp4"
             audioname=f"{yt_title_converter(Filename)}.mp3"
-        print(f"{math_stuff.HumanBytes.format(stream.filesize_approx + audio.filesize_approx)} | Will now be downloaded!")
+        try:
+            print(f"{math_stuff.HumanBytes.format(stream.filesize_approx + audio.filesize_approx)} | {Main[1]}")
+        except:
+            print(Main[4])
+            return "Err1NQ"
         stream.download(filename=Raw_Filename,output_path=f"{root}/Downloads")
         if split == True:
             audio.download(filename=audioname,output_path=f"{root}/Downloads")
@@ -110,12 +127,13 @@ def Download(url,AudioOnly=type(None),UseCaptions=type(None),CaptionsLanguage="e
             os.remove(f"{root}/Downloads/{Raw_Filename}") #Deletes the mute videofile
             os.remove(f"{root}/Downloads/{audioname}") #Deletes the audiofile
         tf = time.perf_counter()
-        print(f"Done in {tf - ts :0.4f} seconds!")
+        timer_result = Main[2].replace("/s",f"{tf - ts:0.4f}")
+        print(timer_result)
     return Filename
 
 def Stream(url,VideoBeta=type(None)): #Video/Audio Streaming
     if VideoBeta == True:
-        print("|WARNING|\n The video option is very unstable, as it's still on development\nTrust me I'm trying my best ¯\_(ツ)_/¯\n also... it has a shitty quality :D ENJOY!")
+        print(Main[5])
         ydl_options = {"format":"bestvideo"}
 
         with youtube_dl.YoutubeDL(ydl_options) as ydl:
@@ -123,17 +141,18 @@ def Stream(url,VideoBeta=type(None)): #Video/Audio Streaming
             url2 = info['formats'][6]['url'] # This is not working well with playlists... but who the hell is going to stream playlists outta this crap!?
             os.system(f"""youtube-dl "{url}" -o - | ffplay - -autoexit""")
     else:
-        print("Loading...")
+        print(Main[8])
         ydl_options = {"format":"bestaudio"}
         with youtube_dl.YoutubeDL(ydl_options) as ydl:
             info = ydl.extract_info(url, download=False)
             url2 = info['formats'][0]['url']
-            print("OK! Enjoy your music!")
+            print(Main[6])
             os.system(f"""ffplay "{url2}" -nodisp -autoexit""")
 
 def PlaylistDownload(playlist, SoundOnly=False): #Downloads playlists using the AutoDownload Method
     p = Playlist(playlist)
-    print(f"Downloading: {p.title}")
+    playlist_title = Main[7].replace("/pt",f"{p.title}")
+    print(playlist_title)
     if SoundOnly == True:
         for video in p.videos:
             safe_title = yt_title_converter(video.title) # Forgot to add this oops
