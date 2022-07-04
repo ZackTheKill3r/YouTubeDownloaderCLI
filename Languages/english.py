f895b310423144cc6258877c32ffd38e39daea1a
Kill3rZack <zackthekill3r@outlook.com>
2022-07-03 22:26:10 -0300

    Language Folder with all pre bundled languages
    Feel free to help me translate this software to any language that you may know!  :)

---
+++ Languages/english.py
@@ -0,0 +1,24 @@
+CLI = ["\nInsert a YouTube Link!\n", #0
+"CLI Version: /cli_version | Core Version: /core_version | A Lot of bugs may occur |", #1
+"\n1 - Download\n2 - Stream\n3 - Exit", #2
+"\n1 - Download\n2 - Exit", #3
+"\nSo, what are we doing?\n", #4
+"Do you want do download only the sound? | Y/N | ", #5
+"\nYour music is ready! do you wanto to play it now? | Y/N | ", #6
+"Enjoy! Press Ctrl+C to stop or wait until the end :D", #7
+"Playing /p", #8
+"Do you want to select the quality | Y/N | ", #9
+"\nIs there anything else you would like to do? | Y/N | ", #10
+"\nGoodbye!", #11
+"Sorry but this function is only avaiable for videos!", #12
+"Do you want to stream the video as well? | *UNSTABLE* | Y/N | "] #13
+
+Main = ["Approximate size:", #0
+"Will now be downloaded!", #1
+"Done in /s seconds!", #2
+"Select your desired quality ", #3
+"Not avaiable | Error Code: Err1nQ |", #4
+"|WARNING|\n The video option is very unsatable, as it's still on development\nTrust me I'm trying my best ¯\_(ツ)_/¯\n also... it has a shitty quality :D ENJOY!",
+"OK! Enjoy your music!", #6
+"Downloading: /pt", #7
+"Loading..."] #8
---
+++ Languages/portuguese.py
@@ -0,0 +1,26 @@
+CLI = ["\nInsira um link do YouTube!\n", #0
+"Versão do CLI: /cli_version | Versão do Núcleo: /core_version | Muitos bugs ainda podem ocorrer |",
+"\n1 - Baixar\n2 - Assistir\n3 - Sair", #2
+"\n1 - Baixar\n2 - Sair", #3
+"\nO que faremos aogra?\n", #4
+"Você quer baixar apenas o áudio? | Y/N | ",#5
+"\nSeu áudio está pronto! Deseja reproduzir-lo agora? | Y/N | ", #6
+"Aproveite! Pressione Ctrl+C para parar ou espere-a acabar :D", #7
+"Tocando /p", #8
+"Deseja selecionar a qualidade? | Y/N | ", #9
+"\nDeseja fazer mais algo? | Y/N | ", #10
+"\nAdeus!", #11
+"Desculpa, mas esta função está disponível apenas para vídeos!", #12
+"Deseja assistir ao vídeo também? | *INSTÁVEL* | Y/N | "] #13
+
+Main = ["Tamanho aproximado:", #0
+"Serão baixados!", #1
+"Baixado em /s segundos!", #2
+"Selecione a qualidade desejada ", #3
+"Não está disponível | Código do erro: Err1nQ ", #4
+"|ATENÇÃO|\n A opção de vídeo é muito instável, pois a mesma ainda está sob desenvolvimento\nConfia em mim estou tentando meu máximo¯\_(ツ)_/¯\n ah... e a qualidade é uma merda :D APROVEITE!",
+"OK! Aproveite seu áudio!", #6
+"Baixando: {/pt", #7
+"Carregando..."] #8
+
+#{tf - ts:0.4f}
