8f476a3c3678c5dbda3e5b3c82812917e8626fde
Kill3rZack <zackthekill3r@outlook.com>
2022-07-03 22:55:12 -0300

    New settings grabber.
    Now containing AutoLang, which comes enabled by default and tries to match the language in your OS, if possible, if not, it will set the language either to English, or one that you selected, it can be disabled through the settings.txt file.

--- settings.py
+++ settings.py
@@ -1,13 +1,30 @@
+import locale
+import ctypes
+windll = ctypes.windll.kernel32
+windll.GetUserDefaultUILanguage()
+
 settings = open("settings.txt", "r")
 language_settings = settings.readline()
+auto_lang = settings.readline()
+os_lang = locale.windows_locale[ windll.GetUserDefaultUILanguage() ]
 
-if "Language = English" in language_settings:
-    language = "English"
-elif "Language = Portuguese" in language_settings:
-    language = "Portuguese"
-elif "Language = Japanese" in language_settings:
-    language = "Japanese"
+if "AutoLang = True" in auto_lang:
+    if "en" in os_lang:
+        language = "English"
+    elif "pt" in os_lang:
+        language = "Portuguese"
+    elif "jp" in os_lang:
+        language = "Japanese"
+    else:
+        language = "English"
 else:
-    language = "English"
+    if "Language = English" in language_settings:
+        language = "English"
+    elif "Language = Portuguese" in language_settings:
+        language = "Portuguese"
+    elif "Language = Japanese" in language_settings:
+        language = "Japanese"
+    else:
+        language = "English"
 
 settings.close()
